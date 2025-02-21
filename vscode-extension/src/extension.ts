bb335189e3c4e10a11ee334a4e1ef9fab30a1a3b
Rishikesh Gharat <rishigharat@gmail.com>
2025-02-21 14:17:17 -0500

    added LICENSE and updated extension.ts with the same

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,3 +1,22 @@
+/**
+ * CodePilotAI - AI-Powered Code Autocompletion for VS Code
+ *
+ * Copyright 2024 [Your Name or Your Project]
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
 import * as vscode from 'vscode';
 import ollama from 'ollama'; // Install via: npm install ollama
 
